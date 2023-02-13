0c07dece745c3555904010d40f9e86f7665ca5e3
Bruno Kempfer Stein <brunokempferstein@MacBook-Pro-de-Bruno.local>
2023-02-13 12:21:04 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import LogoSVG from '../assets/logo-ignite.svg'
+import Image from 'next/image';
+import { Container, Header } from '@/styles/pages/app';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoSVG} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
